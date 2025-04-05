a13b683644c9e125d831ccde2b1eae05a1e18a9a
DevNickjr <nicholasduadei14@gmail.com>
2025-04-05 01:30:24 +0100

    hero

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Fira_Code } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const firaCodeFont = Fira_Code({ subsets: ['latin'] })
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${firaCodeFont.className} text-white antialiased`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,103 @@
+"use client"
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import HeroImg from "@/assets/hero.svg";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <>
+      <section className="relative grid lg:h-screen lg:grid-cols-2 p-8 md:px-16 lg:px-24">
+        <div className="flex flex-col items-start justify-center gap-y-6 z-1 max-w-lg">
+          <h1 className="text-xl text-white lg:text-3xl mb-1">
+            Nicholas is a <span className="text-primary">web designer</span> and <span className="text-primary">front-end developer</span>  
+          </h1>
+          {/* <span className="text-4xl font-bold text-gray-900 lg:text-6xl"></span> */}
+          <span className="text-secondary text-md lg:text-lg">He crafts responsive websites where technologies meet creativity</span>
+          <Button className="">
+            Contact Me!!
+          </Button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div className="flex flex-col justify-center items-center">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src={HeroImg}
+            alt="Hero image"
+            width={200}
+            height={200}
+            className="object-cover w-full aspect-auto object-bottom max-w-xl"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <div className="flex items-center text-xs lg:text-sm gap-3 text-secondary border border-[#ABB2BF] px-2.5 p-1.5"><span className="w-3 h-3 bg-primary"></span> Currently working on <span className="text-white">Portfolio</span></div>
+        </div>
+        {/* <Image
+          src={'/file.svg'}
+          alt="BG image"
+          width={200}
+          height={200}
+          className="absolute top-0 left-0 object-contain object-center w-full h-full px-12 py-12 -z-10 transitio"
+        /> */}
+      </section>
+      <section className="py-20 p-8 md:px-16 lg:px-24 flex flex-col gap-5 items-center text-center">
+        <Image
+          src={HeroImg}
+          alt="Hero image"
+          width={200}
+          height={200}
+          className="object-cover h-60 w-60 rounded-full aspect-auto object-bottom mb-3"
+        />
+        <h2>About Me</h2>
+        <div className="w-full h-[1px] scale-x-2 bg-secondary/50"></div>
+        <p className="text-lg lg:text-xl">6 Years of experience building the web.</p>
+        <p className="text-xs lg:text-sm text-secondary max-w-2xl">Over eight years in business, I have had a chance to work on a variety of projects, with agencies, companies or even individuals.Branding, web design, UI/UX design and front-end development. For work inquires send me an Email</p>
+      </section>
+      {/* <motion.div
+        style={{ transformOrigin: "bottom right" }}
+        animate={{ scale: 1.5 }}
+        transition={{ duration: 0.5 }}
+      >
+        Scale from Bottom Right
+      </motion.div> */}
+      <div className="flex flex-col gap-6 p-6">
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+        <span>fdgfhjgf</span>
+      </div>
+    </>
   );
 }
